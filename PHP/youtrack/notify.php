7d61ea527e6ebf4041a0b6825cff2913ca79d208
Aurélien Lavorel <aurelien@lavoweb.net>
2021-12-28 13:53:42 +0100

    add youtrack notify

---
+++ PHP/youtrack/notify.php
@@ -0,0 +1,60 @@
+<?php
+$token = getenv('YOUTRACK_TOKEN');
+$apiDomain = getenv('YOUTRACK_DOMAIN');
+$commitMessage = getenv('CI_COMMIT_MESSAGE');
+$jobUrl = getenv('CI_JOB_URL');
+$youtrackGroupId = getenv('YOUTRACK_GROUP_ID');
+$env = getenv('ENVIRONMENT');
+$regex = '#[A-Z0-9]+-[0-9]+#';
+
+if (!$commitMessage) {
+    die("No commit found \n");
+}
+
+function createComment($apiDomain, $token, $issueId, $data)
+{
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL, $apiDomain . 'api/issues/' . $issueId . '/comments');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+
+    $headers = array();
+    $headers[] = 'Accept: application/json';
+    $headers[] = 'Authorization: Bearer ' . $token;
+    $headers[] = 'Content-Type: application/json';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    } else {
+        echo 'Comment added';
+    }
+    curl_close($ch);
+}
+
+preg_match_all($regex, $commitMessage, $res);
+$res = array_unique($res[0]);
+
+if (!count($res)) {
+    die("No commit found \n");
+}
+
+foreach ($res as $issueId) {
+    $message = '{"text": "Déployé en ' . $env . '"}';
+    createComment($apiDomain, $token, $issueId, $message);
+    $message = '{
+        "text": "Log: ' . $jobUrl . '"
+        "visibility": {
+            "permittedGroups": [
+                {"id": "' . $youtrackGroupId . '"}
+            ],
+            "$type": "LimitedVisibility"
+        }
+    }';
+    createComment($apiDomain, $token, $issueId, $message);
+}
+echo 'Done';
+echo "\n";
