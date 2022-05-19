528d31b0ef0d696360c43314b0418fd93bcd78bb
Aurélien Lavorel <aurelien@lavoweb.net>
2022-05-19 17:35:56 +0200

    retry gitlab canceled jobs

--- PHP/youtrack/notify.php
+++ PHP/youtrack/notify.php
@@ -6,9 +6,55 @@ $jobUrl = getenv('CI_JOB_URL');
 $youtrackGroupId = getenv('YOUTRACK_GROUP_ID');
 $env = getenv('ENVIRONMENT');
 $regex = '#[A-Z0-9]+-[0-9]+#';
+$projectId = getenv('$CI_PROJECT_ID');
+$apiDomain = getenv('CI_API_V4_URL') . '/';
+$token = getenv('CI_JOB_TOKEN');
 
 if (!$commitMessage) {
     die("No commit found \n");
+}
+
+function retryJob($jobId, $projectId, $apiDomain, $token) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $apiDomain . 'projects/' . $projectId . '/jobs/' . $jobId . '/retry');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_POST, 1);
+
+    $headers = array();
+    $headers[] = 'Accept: application/json';
+    $headers[] = 'PRIVATE-TOKEN: ' . $token;
+    $headers[] = 'Content-Type: application/json';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+}
+
+function retryCanceledJobs($projectId, $apiDomain, $token) {
+    $dateLastWeek = strtotime(date("Y-m-d", strtotime(date("Y-m-d"))) . " -1 week");
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $apiDomain . 'projects/' . $projectId . '/jobs?scope[]=canceled');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $headers = array();
+    $headers[] = 'Accept: application/json';
+    $headers[] = 'PRIVATE-TOKEN: ' . $token;
+    $headers[] = 'Content-Type: application/json';
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $result = curl_exec($ch);
+    if (curl_errno($ch)) {
+        echo 'Error:' . curl_error($ch);
+    } else {
+        echo $result . "\n";
+        $jobs = json_decode($result, true);
+        foreach ($jobs as $job) {
+            if ($job['stage'] == 'notify' && $job['pipeline']['status']  == 'canceled' && !($job['finished_at'] < $dateLastWeek)) {
+                retryJob($job['id'], $projectId, $apiDomain, $token);
+            }
+        }
+        echo 'Old jobs launched';
+    }
+    curl_close($ch);
 }
 
 function createComment($apiDomain, $token, $issueId, $data)
@@ -57,5 +103,6 @@ foreach ($res as $issueId) {
     $message = json_encode($data);
     createComment($apiDomain, $token, $issueId, $message);
 }
+retryCanceledJobs($projectId, $apiDomain, $token);
 echo 'Done';
 echo "\n";
